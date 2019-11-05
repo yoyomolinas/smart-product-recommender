3e81d7ab27cf87801867824aadb4bda3068b2bf5
yoyomolinas <molinas.yoel@gmail.com>
2019-11-05 15:32:49 +0000

    triplet generator function; triplet loss; class based train test splitter function;

---
+++ imageFeatureExtractor/utils.py
@@ -0,0 +1,56 @@
+import random
+import numpy as np
+
+def split(x, y, ratio):
+    """
+    Split classes as test/train
+    Return x_train, x_test, y_train, y_test
+    """
+    random.seed(2)
+    classes, counts = np.unique(y, return_counts=True)
+    test_cls = random.sample(list(classes), int(len(classes) * ratio))
+    train_cls = list(set(classes).difference(set(test_cls)))
+
+    # Split X/Y for training and testing
+    train_index = list(filter(lambda i: y[i] in train_cls, range(len(y))))
+    test_index = list(filter(lambda i: y[i] in test_cls, range(len(y))))
+    return x[train_index], x[test_index], y[train_index], y[test_index]
+
+def generate_triplet(x, y, ap_pairs=10, an_pairs=10):
+    """
+    Triplet generation for given images (x), and classes (y). A triplet consists of an anchor, a positive and negative. 
+    The idea is to predict features that have the anchor closer to the positive and farther from the negative.
+    :param x: array of images 
+    :param y: array of class ids
+    :return tuple: tuple of arrays of anchor, positives, and negatives
+    """
+    data_xy = tuple([x,y])
+
+    triplet_pairs = []
+    anchor = []
+    pos = []
+    neg = []
+    for data_class in tqdm(sorted(set(data_xy[1]))):
+        same_class_idx = np.where((data_xy[1] == data_class))[0]
+        diff_class_idx = np.where(data_xy[1] != data_class)[0]
+        num_perms = len(list(permutations(same_class_idx,2)))
+        if num_perms < 2 or num_perms < ap_pairs:
+            continue
+        
+        A_P_pairs = random.sample(list(permutations(same_class_idx,2)),k=ap_pairs) #Generating Anchor-Positive pairs
+        Neg_idx = random.sample(list(diff_class_idx),k=an_pairs)
+
+        A_P_len = len(A_P_pairs)
+        Neg_len = len(Neg_idx)
+        for ap in A_P_pairs[:A_P_len]:
+            Anchor = data_xy[0][ap[0]]
+            Positive = data_xy[0][ap[1]]
+            for n in Neg_idx:
+                Negative = data_xy[0][n]
+                anchor.append(Anchor)
+                pos.append(Positive)
+                neg.append(Negative)
+                
+    # return np.array(triplet_pairs)
+    return anchor, pos, neg
+
