c48395dee92260986dcb4d0b3f9e74d382eb0da7
Emre Uludag <emre.uludag@useinsider.com>
2019-12-04 20:30:27 +0300

    modified server and mobile application, now our api is ready

--- flaskServer/api/fetcher.py
+++ flaskServer/api/fetcher.py
@@ -1,10 +1,24 @@
 from .models import MatchingProduct
-def fetchClostestImages(imageData):
+def fetchClostestImages(imageData,matchingid):
+    """
+    This imageData object will be in the form of :
+    id, imageBase64String, minPrice, maxPrice  
+    """
     returned_products = []
+    
+    #TODO We must fill the above array with 10 closest images. You have three inputs from imageData object:
+    # imageBase64String
+    # minPrice
+    # maxPrice
+    print("Image data is the following:")
+    print(matchingid)
+
     for i in range(10):
+        
         print("I am getting Yoel's images (This is my bro Yoel's part) ")    
-        new_product = MatchingProduct(matching_id=imageData['id'], imageUrl='www.dummy.com/'+i, productUrl='www.dummy.com/'+i])
+        new_product = MatchingProduct(matching_id=matchingid, imageUrl='www.dummy.com/'+str(i), productUrl='www.dummy.com/'+str(i))
         returned_products.append(new_product)
+    
     return returned_products 
 
     
--- smartProductRecommenderMobileApp/App.js
+++ smartProductRecommenderMobileApp/App.js
@@ -1,62 +1,63 @@
-import React, {useEffect,useState} from 'react';
+import React, {Component,useEffect,useState} from 'react';
 import {StyleSheet, View} from 'react-native';
+import RecommendationScreen from './screens/RecommendationScreen';
 import Header from './components/Header';
 import MainPage from './screens/MainPage';
 import ProductShot from './screens/ProductShot';
 import ImgPicker from './screens/ImgPicker';
-import RecommendationScreen from './screens/RecommendationScreen';
+export default class App extends Component {
+
+    // const [userName, setUserName] = useState();
+    // const [cameraMode, setCameraMode] = useState();
+    // const [imageData, setImageData] = useState();
+    // const [max_price, setMax] = useState();
+    // const [min_price, setMin] = useState();
+    //
+    // const mainPageHandler = selectedName => {
+    //     setUserName(selectedName);
+    // };
+    //
+    // const cameraHandler = cameraMode => {
+    //     setCameraMode(cameraMode);
+    // };
+    // const maxPriceHandler = maxPrice => {
+    //     setMax(maxPrice);
+    // };
+    // const minPriceHandler = minPrice => {
+    //     setMin(minPrice);
+    // };
+    // const imageDataHandler = imageData => {
+    //     setImageData(imageData);
+    // };
+    //
+    //
+    // let content = <MainPage onMainPageLoad={mainPageHandler}/>;
+    //
+    // if (userName) {
+    //     content = <ProductShot nameOfUser={userName} onCameraPageClick={cameraHandler}/>;
+    // }
+    //
+    // if (cameraMode) {
+    //     content = <ImgPicker onSetMax={maxPriceHandler} onSetMin={minPriceHandler}
+    //                          onImageData={imageDataHandler}/>
+    // }
+    //
+    // if (max_price && min_price && imageData) {
+    //     console.log(imageData.substring(0, 10));
 
 
-
-export default function App() {
-    const [userName, setUserName] = useState();
-    const [cameraMode, setCameraMode] = useState();
-    const [imageData, setImageData] = useState();
-    const [max_price, setMax] = useState();
-    const [min_price, setMin] = useState();
-
-    const mainPageHandler = selectedName => {
-        setUserName(selectedName);
-    };
-
-    const cameraHandler = cameraMode => {
-        setCameraMode(cameraMode);
-    };
-    const maxPriceHandler = maxPrice => {
-        setMax(maxPrice);
-    };
-    const minPriceHandler = minPrice => {
-        setMin(minPrice);
-    };
-    const imageDataHandler = imageData => {
-        setImageData(imageData);
-    };
+    content = <RecommendationScreen/>;
 
 
-    let content = <MainPage onMainPageLoad={mainPageHandler}/>;
-
-    if (userName) {
-        content = <ProductShot nameOfUser={userName} onCameraPageClick={cameraHandler}/>;
+    render() {
+        return (
+            <View style={styles.screen}>
+                <Header title="Koc University"/>
+                {this.content}
+            </View>
+        );
     }
-
-    if (cameraMode) {
-        content = <ImgPicker onSetMax={maxPriceHandler} onSetMin={minPriceHandler}
-                             onImageData={imageDataHandler}/>
-    }
-
-    if (max_price && min_price && imageData) {
-        console.log(imageData.substring(0, 10));
-        content = <RecommendationScreen/>
-    }
-
-    return (
-        <View style={styles.screen}>
-            <Header title="Koc University"/>
-            {content}
-        </View>
-    );
 }
-
 const styles = StyleSheet.create({
     screen: {
         flex: 1
