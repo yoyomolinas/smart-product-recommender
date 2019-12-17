221dc4b711676063e575b00d545eaeac2c3fb4f4
Emre <uludag.emre.tr@gmail.com>
2019-12-17 19:53:06 +0300

    fixed benchmark project is 100% ready

--- flaskServer/api/adminPage/js/main.js
+++ flaskServer/api/adminPage/js/main.js
@@ -3,10 +3,12 @@ document.getElementsByClassName("login100-form-btn")[0].addEventListener("click"
 function clickButton(){
   if(document.getElementsByName("username")[0].value=='emre' && document.getElementsByName("pass")[0].value=='emre'){
     var link = 'http://basic4ppc.com:51042/reports/google_charts.html'; //our target link goes here;
-    document.write('<html><body style="margin:0px;padding:0px;overflow:hidden" ><iframe src="'+link+'"  frameborder="0" style="overflow:hidden;height:100%;width:100%" height="100%" width="100%"></iframe></body></html>');
+    document.write('<html><body style="margin:0px;padding:0px;overflow:hidden" ><embed src="Deep_Fashion_Results.pdf" width="100%" height="100%" /></body></html>');
     console.log("You pushed the right button")
   }else{
     alert("Unsuccessful Login");
   }
 
 };
+
+//<iframe src="'+link+'"  frameborder="0" style="overflow:hidden;height:100%;width:100%" height="100%" width="100%"></iframe>
